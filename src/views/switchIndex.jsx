9942075bf2ff22a958990de31ddd86b60f032b95
yangyuecoding <951921220@qq.com>
2020-07-24 22:50:07 +0800

    qidian beat.0.0.2

--- src/views/switchIndex.jsx
+++ src/views/switchIndex.jsx
@@ -24,6 +24,7 @@ export default class switchIndex extends Component {
         })
         .catch( rej => {
             console.log(rej);
+            console.log("hello");
         })
     }
     componentDidMount(){
